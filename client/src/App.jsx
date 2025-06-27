513dbdc080c43be7991bf11916ecc5edfcbfb8ae
Divine Precious-Esue <divinepreciousesue@gmail.com>
2025-06-27 13:40:30 -0700

    Installed TailwindCSS and deleted more templates

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,11 +2,10 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Random</h1>
+      <h1 className="text-3xl">Random</h1>
     </>
   )
 }
