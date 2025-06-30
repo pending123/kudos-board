83f5919d16492400acc84e00eb8027930b2383e9
Divine Precious-Esue <divinepreciousesue@gmail.com>
2025-06-30 14:41:50 -0700

    Added cardRoutes to server.js

--- server/server.js
+++ server/server.js
@@ -9,9 +9,11 @@ const corsOption = {
   origin: "http://localhost:5173",
 };
 
-const boardRoutes = require("./routes/boardRoutes")
+const boardRoutes = require("./routes/boardRoutes");
+const cardRoutes = require("./routes/cardRoutes");
 
 app.use(cors(corsOption));
-app.use("/boards", boardRoutes)
+app.use("/boards", boardRoutes);
+app.use("/", cardRoutes);
 
 app.listen(3000, () => console.log("Server running on port 3000"));
