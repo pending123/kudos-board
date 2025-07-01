f76a435155dc37356188575761c3584758e0775e
Divine Precious-Esue <divinepreciousesue@gmail.com>
2025-07-01 15:04:37 -0700

    Fixed merge conflicts

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,27 +1,38 @@
 import {useState, useEffect} from "react";
 import axios from "axios";
 import BoardGrid from "../components/BoardGrid"
+import SubNavbar from "../components/subNavbar";
 
 export default function Home() {
     const [boards, setBoards] = useState([]);
 
+    const fetchBoards = async () => {
+        try {
+            const { data } = await axios.get("http://localhost:3000/boards");
+            setBoards(data);
+        } catch (err) {
+            console.log("Error fetching boards:", err);
+        }
+    };
 
     useEffect(() => {
-        const fetchBoards = async () => {
-            try {
-                const { data } = await axios.get("http://localhost:3000/boards");
-                setBoards(data);
-            } catch (err) {
-                console.log("Error fetching boards:", err);
-            }
-        };
-
         fetchBoards();
     }, []);
     
     return (
         <>
-         <BoardGrid boards={boards}/>
+        <div className="navBar">
+        <SubNavbar
+        activeCategory={activeCategory}
+        setActiveCategory={setActiveCategory}
+        searchInputValue={searchInputValue}
+        handleOnSearchInputChange={handleOnSearchInputChange}
+        handleSearchSubmit={handleSearchSubmit}
+        handleClearSearch={handleClearSearch}
+        />
+        <p>Welcome to Home Page!</p>
+        </div>
+         <BoardGrid boards={boards} fetchBoards={fetchBoards}/>
         </>
     )
 }
