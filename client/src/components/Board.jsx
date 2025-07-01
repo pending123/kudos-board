c3fc89909828d643c8c606ef25b7c44f961e44d2
Divine Precious-Esue <divinepreciousesue@gmail.com>
2025-06-30 20:55:29 -0700

    Completed First Draft of Board Design

--- client/src/components/Board.jsx
+++ client/src/components/Board.jsx
@@ -1,6 +1,9 @@
+import { useState } from "react"
+
 export default function Board({boardId, title, category, author, image}) {    
     
     return (
+        <>
         <div className="flex flex-col items-center border border-black pb-4">
             <img src="https://preview.redd.it/i-got-bored-so-i-decided-to-draw-a-random-image-on-the-v0-4ig97vv85vjb1.png?width=1080&crop=smart&auto=webp&s=28c3ad73cff636f7ba478a0c19d734cd538949d4" />
             <h1 className="text-4xl mt-3">{title}</h1> 
@@ -10,5 +13,6 @@ export default function Board({boardId, title, category, author, image}) {
                 <button className="text-2xl border border-black p-1.5 cursor-pointer">Delete Board</button>
             </div>
         </div>
+        </>
     )
 }
